9376ba113d4b1a0cc6cce726a875d1cb8a0c82f3
SantiagoMena <santiagomenape@gmail.com>
2024-03-23 00:54:57 -0300

    Horizon queue ExecuteTotal

--- config/horizon.php
+++ config/horizon.php
@@ -182,7 +182,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => ['default', 'ExecuteTotal'],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 1,
