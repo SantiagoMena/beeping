8a7ee31b24b1a88bc84c989333545b0a154fd871
SantiagoMena <santiagomenape@gmail.com>
2024-03-22 22:07:15 -0300

    OrderLine Factory

---
+++ database/factories/OrderLineFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderLine>
+ */
+class OrderLineFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'qty' => fake()->randomDigitNotNull(),
+            'order_id' => Order::factory(),
+            'product_id' => Product::factory(),
+        ];
+    }
+}
